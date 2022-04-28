a6122c257067cd456bc0de1fb58310ecf1771773
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2022-04-28 10:13:08 +0300

    Update CurrencyPageParameters.cs

--- Models/CurrencyPageParameters.cs
+++ Models/CurrencyPageParameters.cs
@@ -1,14 +1,14 @@
 ﻿namespace WebApiCurrency.Models
 {
-public class CurrencyPageParameters
-{
-    public const int MaxPageSize = 5;
-    public int PageNumber { get; set; } = 1;
-    public int _pageSize;
-    public int PageSize
+    public class CurrencyPageParameters
     {
-        get => _pageSize;
-        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        public const int MaxPageSize = 5;
+        public int PageNumber { get; set; } = 1;
+        public int _pageSize;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
     }
-}
 }
